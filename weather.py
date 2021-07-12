bca85e54efaaf7853b3ded46fea35ef11fd3a1ea
Nitesh balla <66734659+Niteshballa@users.noreply.github.com>
2021-07-12 19:53:37 +0530

    Add files via upload

--- weather.py
+++ weather.py
@@ -8,52 +8,53 @@ API_KEY = "967ceca5dfb9d8a4dba23010c6e885a3"
 print('1 -> Get Weather\n2 -> Get Wind Speed\n3 -> Get Pressure\n0 -> Exit')
 OPTION = int(input())
 while OPTION != 0:
-    print("Enter City")
-    CITY = input()
-    print("Enter Day-Month-Year")
-    date=input().split("-")
-    day=date[0]
-    month=date[1]
-    year=date[2]
-    finaldate= year+"-"+month+"-"+day
-    print(finaldate)
-    URL = BASE_URL + "q=" + CITY + "&units=metric&appid=" + API_KEY
-    response = requests.get(URL)
-    # print(response)
-    # getting data in the json format
-    data = response.json()
-    # print(data)
-    for item in data['list']:
-        time = item['dt_txt']
-        next_date, hour = time.split(' ')
-        # print(next_date)
-        if finaldate == next_date:
+    if OPTION!=1 or OPTION!=2 or OPTION!=3  or OPTION!=0:
+        print("Invalid Input")
+    else:
+        print("Enter City")
+        CITY = input()
+        print("Enter Day-Month-Year")
+        date=input().split("-")
+        day=date[0]
+        month=date[1]
+        year=date[2]
+        finaldate= year+"-"+month+"-"+day
+        print(finaldate)
+        URL = BASE_URL + "q=" + CITY + "&units=metric&appid=" + API_KEY
+        response = requests.get(URL)
+        # print(response)
+        # getting data in the json format
+        data = response.json()
+        # print(data)
+        for item in data['list']:
+            time = item['dt_txt']
+            next_date, hour = time.split(' ')
+            # print(next_date)
+            if finaldate == next_date:
             # getting the main dict block
-            main = item['main']
+                main = item['main']
             # getting temperature
-            temperature = main['temp']
+                temperature = main['temp']
                     
                     # weather report
-            if OPTION == 1:
+                if OPTION == 1:
                 # report = item['weather']
                 # print(item['weather'][0]['description'])
-                print(f"Weather Report: {item['weather'][0]['description']}\nTemperature(Cel): {item['main']['temp']}")
-                break;
-            elif OPTION == 2:
-                wind = item['wind']
-                speed = wind['speed']
-                print(f"Wind Speed: {speed}")
-                break;
-            elif OPTION == 3:
-                # getting the pressure
-                pressure = main['pressure']
-                print(f"Pressure(hPa): {pressure}")
-                break;
+                    print(f"Weather Report: {item['weather'][0]['description']}\nTemperature(Cel): {item['main']['temp']}")
+                    break;
+                elif OPTION == 2:
+                    wind = item['wind']
+                    speed = wind['speed']
+                    print(f"Wind Speed: {speed}")
+                    break;
+                elif OPTION == 3:
+                    # getting the pressure
+                    pressure = main['pressure']
+                    print(f"Pressure(hPa): {pressure}")
+                    break;
 
                 # else :
                 #     # showing the error message
                 #     print("Error in the HTTP request")
-            else:
-                print("Invalid Input")
     print('1 -> Get Weather\n2 -> Get Wind Speed\n3 -> Get Pressure\n0 -> Exit')
     OPTION = int(input())
